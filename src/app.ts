d67ecbafad7b1f9f6b02945fe3e76d15521b8964
nukhaimong <nukhaimongmarma834@gmail.com>
2026-03-18 16:38:00 +0600

    setup: setup catchAsync, sendResponse notFound and IndexRoutes for the project

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application } from 'express';
 import cookieParser from 'cookie-parser';
 import { globalErrorHandler } from './app/middleware/globalErrorHanlder';
+import { notFound } from './app/middleware/notFound';
+import { IndexRoutes } from './app/router';
 
 const app: Application = express();
 app.use(express.json());
@@ -8,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+app.use('/api/v1/', IndexRoutes);
+
 app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
