2c5b55017512df9d61bc7e1513fbfba36501c431
nukhaimong <nukhaimongmarma834@gmail.com>
2026-04-08 03:15:54 +0600

    try to redeploy

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { envVars } from '../../config/env.js';
-import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaClient } from '@prisma/client';
 
 const connectionString = `${envVars.DATABASE_URL}`;
 
